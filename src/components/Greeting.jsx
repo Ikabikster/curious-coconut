be044d1b33c1cccd930991730a53590eb1fb4e80
ikabikster <stefanie.weiss@posteo.de>
2025-07-26 21:33:20 +0200

    CHANGE: clear inputField

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -7,27 +7,29 @@ const Greeting = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         setName(inputName);
+        setInputName("");
     }
 
     return (
         <div style={{textAlign: 'center'}} className="greeting">
-            <h4>Hey, {name}</h4>
             <form onSubmit={handleSubmit}>
                 <div style={{marginTop: "20px"}}>
-                <label-left htmlFor="name">What may I call you? </label-left>
-                <input
-                    className="inputField"
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={inputName}
-                    required="required"
-                    onChange={e => setInputName(e.target.value)}
-                />
+                    <label-left htmlFor="name">What may I call you? </label-left>
+                    <input
+                        className="inputField"
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={inputName}
+                        required="required"
+                        onChange={e => setInputName(e.target.value)}
+                    />
                 </div>
                 <br/>
                 <input className="inputField" type="submit" value="Ok"/>
             </form>
+            <br/>
+            <h1>Hey, {name}!</h1>
         </div>
     )
 }
