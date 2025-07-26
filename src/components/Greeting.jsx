3d54a3056330ac9738983bc341c2aac622271e5e
ikabikster <stefanie.weiss@posteo.de>
2025-07-26 21:44:00 +0200

    CHANGE: optics

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -14,19 +14,19 @@ const Greeting = () => {
         <div style={{textAlign: 'center'}} className="greeting">
             <form onSubmit={handleSubmit}>
                 <div style={{marginTop: "20px"}}>
-                    <label-left htmlFor="name">What may I call you? </label-left>
+                    <label-left className="labelLeft" htmlFor="name">What may I call you?  </label-left>
                     <input
                         className="inputField"
                         type="text"
                         id="name"
                         name="name"
                         value={inputName}
+                        placeholder="Your name"
                         required="required"
                         onChange={e => setInputName(e.target.value)}
                     />
+                <input className="submitButton" type="submit" value="Ok"/>
                 </div>
-                <br/>
-                <input className="inputField" type="submit" value="Ok"/>
             </form>
             <br/>
             <h1>Hey, {name}!</h1>
