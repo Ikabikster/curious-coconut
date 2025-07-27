fe5194e05d855722692accfd135564234c192d28
ikabikster <stefanie.weiss@posteo.de>
2025-07-27 22:24:50 +0200

    CHANGE Greeting

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,18 +3,20 @@ import { useState } from 'react';
 const Greeting = () => {
     const [name, setName] = useState("Stranger");
     const [inputName, setInputName] = useState("");
+    const [textDisplay, setTextDisplay] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setName(inputName);
         setInputName("");
+        setTextDisplay(true);
     }
 
     return (
         <div style={{textAlign: 'center'}} className="greeting">
             <form onSubmit={handleSubmit}>
                 <div style={{marginTop: "20px"}}>
-                    <label-left className="labelLeft" htmlFor="name">What may I call you?  </label-left>
+                    <label-left className="labelLeft" htmlFor="name">What may I call you?</label-left>
                     <input
                         className="inputField"
                         type="text"
@@ -25,11 +27,15 @@ const Greeting = () => {
                         required="required"
                         onChange={e => setInputName(e.target.value)}
                     />
-                <input className="submitButton" type="submit" value="Ok"/>
+                    <input className="submitButton" type="submit" value="Ok"/>
                 </div>
             </form>
             <br/>
             <h1>Hey, {name}!</h1>
+            {textDisplay
+                ? <p>How nice to see you :)</p>
+                : <></>}
+
         </div>
     )
 }
